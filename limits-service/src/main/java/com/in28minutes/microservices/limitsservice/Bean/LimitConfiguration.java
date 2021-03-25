68674a0b7ed7b79f1444ff8d5e6e51ca2b92587e
Marcos Arce <marcosarce04@gmail.com>
2021-03-25 11:24:57 -0300

    Initial commit
    Initial commit with Limits Microservice (properties configuration for maximum and minimum)

---
+++ limits-service/src/main/java/com/in28minutes/microservices/limitsservice/Bean/LimitConfiguration.java
@@ -0,0 +1,24 @@
+package com.in28minutes.microservices.limitsservice.Bean;
+
+public class LimitConfiguration {
+
+    private int maximum;
+    private int minimum;
+
+    public LimitConfiguration() {
+    }
+
+    public LimitConfiguration(int maximum, int minimum) {
+        this.maximum = maximum;
+        this.minimum = minimum;
+    }
+
+    public int getMaximum() {
+        return maximum;
+    }
+
+    public int getMinimum() {
+        return minimum;
+    }
+
+}
---
+++ limits-service/src/main/java/com/in28minutes/microservices/limitsservice/Controllers/LimitsConfigurationController.java
@@ -0,0 +1,20 @@
+package com.in28minutes.microservices.limitsservice.Controllers;
+
+import com.in28minutes.microservices.limitsservice.Bean.LimitConfiguration;
+import com.in28minutes.microservices.limitsservice.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsConfigurationController {
+
+    @Autowired
+    private Configuration configuration;
+
+
+    @GetMapping("/limits")
+    public LimitConfiguration retrieveLimitsFromConfiguration() {
+        return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+    }
+}
